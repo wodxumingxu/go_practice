f55e74867950b92169dfdde3049df677cee2d9de
wodxumingxu <wodxuming@gmail.com>
2018-09-04 16:52:44 -0400

    Create uniqueMorseCodeWords.go

---
+++ Unique Morse Code Words/uniqueMorseCodeWords.go
@@ -0,0 +1,22 @@
+import ("fmt"
+       "bytes")
+       
+func uniqueMorseRepresentations(words []string) int {
+    
+    mors := []string{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."}
+    
+    
+    morsMap := make(map[string] int)
+    for _, word := range words {
+        
+        var buffer string
+        
+        for _, c := range word {
+            buffer += mors[c - 'a']
+        }
+        morsMap[buffer] += 1
+    }
+    
+    return len(morsMap)
+    
+}
